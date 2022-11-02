22e198948e1f4dac14d912740393d66227afdb8a
Boston Lowrey <bostonlowrey@gmail.com>
2022-11-02 16:24:37 -0400

    Add array methods and explinations

--- arrays.rb
+++ arrays.rb
@@ -1,9 +1,14 @@
 # variables
 
 names = ["Boston", "Zoe", "Levi", "Stephanie"]
-
+names.pop
+# pop will remove the last elemnt of the array.
 random_nums = [1, 2, 3, 4]
-
+random_nums.shift 
+# shift removes the first element of the array.
 random_decimals = [1.5, 2.5, 3.5, 4.5]
-
+random_decimals.rotate!(2)
+#rotate! rotates whatever element is chosen to the start of the array.
 true_&_falses = [true, false, true, false]
+true_&_falses.sample
+#sample will pick a random element in the array.
